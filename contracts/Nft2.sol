eca378c83a3241f67935316a26c972c0e120a7c8
shawn ning <zxning@gmail.com>
2022-09-20 11:43:47 -0400

    update

--- contracts/Nft2.sol
+++ contracts/Nft2.sol
@@ -6,26 +6,68 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+struct Rec { 
+    address payable owner;
+    uint expiredAt;
+}
 
 contract Nft2 is ERC1155, Ownable {
-     using Counters for Counters.Counter;
-     Counters.Counter private _tokenIds;
-     mapping(uint256 => string) private _tokenURIs;
-     constructor() ERC1155("") {}
-     function _setTokenURI(uint256 tokenId, string memory _tokenURI)
-     internal
-     virtual
-     {
-        _tokenURIs[tokenId] = _tokenURI;
-     }
-     function mintNFT(address recipient, string memory _tokenURI)
-     public onlyOwner
-     returns (uint256)
-     {
-       _tokenIds.increment();
-       uint256 newItemId = _tokenIds.current();
-       _mint(recipient, newItemId, 1, "");
-       _setTokenURI(newItemId, _tokenURI);
-       return newItemId;
-     }
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    mapping(uint256 => string) private _tokenURIs;
+    
+    uint rate;
+    //mapping(string => Rec) public hosts; 
+    mapping(string => uint256) public hosts; 
+
+    constructor(uint _rate, string memory _baseURI) ERC1155(_baseURI) {
+        rate = _rate;
+    }
+
+    function mintNFT(string memory host, uint numOfDays) public payable returns (uint256) {
+        require(msg.value >= numOfDays*rate, "insufficient funds");
+
+
+/*
+        require(bytes(_host).length > 1, "host length must be greater than 1");
+        //console.log(msg.value, numOfDays*rate);
+        require(msg.value >= numOfDays*rate, "insufficient funds");
+
+        if (hosts[_host].expiredAt > 0){    //exist record
+            console.log("existing record");
+
+            if (block.timestamp > hosts[_host].expiredAt){
+                hosts[_host] = Rec(_host, payable(msg.sender), block.timestamp + numOfDays*24*60*60);
+            }
+            else{
+                require(msg.sender == hosts[_host].owner, "You aren't the owner and record is not expired yet");
+                hosts[_host].expiredAt = hosts[_host].expiredAt + numOfDays*24*60*60;
+            }
+        }
+        else{
+            console.log("new record");
+            hosts[_host] = Rec(_host, payable(msg.sender), block.timestamp + numOfDays*24*60*60);
+        }
+
+
+ */
+
+
+
+
+
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId, 1, "");
+
+        hosts[host] = newItemId;
+        return newItemId;
+    }
+
+    function uri(uint256 id) public view override returns (string memory) {
+        //return string(bytes.concat(bytes(super.uri(id)), "/", bytes(Strings.toString(id)), ".json"));
+        return string(abi.encodePacked(super.uri(id), Strings.toString(id), ".json" ));
+    }
 }
--- test/Nft2.js
+++ test/Nft2.js
@@ -6,22 +6,32 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 const TWEI = 1_000_000_000_000;
+const RATE = 100*TWEI;
 
 describe("Nft2", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
   async function deployFixture() {
     const [owner, alice, bob] = await ethers.getSigners();
 
     const Nft2 = await ethers.getContractFactory("Nft2");
-    const nft2 = await Nft2.deploy();
+    //const nft2 = await Nft2.deploy(RATE, "https://localhost:3000/{id}.json");
+    const nft2 = await Nft2.deploy(RATE, "https://localhost:3000/");
     return { nft2, owner, alice, bob };
   }
 
   describe("Deployment", function () {
-    it("Should set the right host", async function () {
+    it("Should work", async function () {
       const { nft2, owner, alice, bob} = await loadFixture(deployFixture);
+
+      const days = 100;
+      await expect(nft2.connect(alice).mintNFT('abc', days, {
+        //from: alice.address,
+        value: ethers.BigNumber.from(RATE).mul(days)
+      })).not.to.be.reverted;
+
+
+      const nftId = await nft2.hosts('abc');
+      const nftURI = await nft2.uri(nftId);
+      console.log(nftURI);
 
     });
   });
