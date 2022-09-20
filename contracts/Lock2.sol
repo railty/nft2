a553e1538e024e32e22cc0a3c75b285f22578516
Shawn Ning <zxning@gmail.com>
2022-09-20 00:40:42 -0400

    update

--- contracts/Lock2.sol
+++ contracts/Lock2.sol
@@ -4,20 +4,43 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 
-contract Lock2 {
-    string public host;
-    address payable public owner;
+struct Rec { 
+    string host;
+    address payable owner;
+    uint expiredAt;
+}
 
-    constructor() payable {
-        host = "_";
-        owner = payable(msg.sender);
+contract Lock2 {
+    uint rate;
+    mapping(string => Rec) public hosts; 
+
+    constructor(uint _rate) {
+        rate = _rate;
     }
 
-    function setHost(string memory _host) public {
-        require(bytes(_host).length > 1, "host length greater than 1");
-        //require(msg.sender == owner, "You aren't the owner");
+    function setHost(string memory _host, uint numOfDays) public payable {
+        require(bytes(_host).length > 1, "host length must be greater than 1");
+        //console.log(msg.value, numOfDays*rate);
+        require(msg.value >= numOfDays*rate, "insufficient funds");
 
-        host = _host;
-        owner = payable(msg.sender);
+        if (hosts[_host].expiredAt > 0){    //exist record
+            console.log("existing record");
+
+            if (block.timestamp > hosts[_host].expiredAt){
+                hosts[_host] = Rec(_host, payable(msg.sender), block.timestamp + numOfDays*24*60*60);
+            }
+            else{
+                require(msg.sender == hosts[_host].owner, "You aren't the owner and record is not expired yet");
+                hosts[_host].expiredAt = hosts[_host].expiredAt + numOfDays*24*60*60;
+            }
+        }
+        else{
+            console.log("new record");
+            hosts[_host] = Rec(_host, payable(msg.sender), block.timestamp + numOfDays*24*60*60);
+        }
+    }
+
+    function getTime() public view returns(uint) {
+        return block.timestamp;
     }
 }
--- test/Lock2.js
+++ test/Lock2.js
@@ -1,9 +1,13 @@
+const moment = require("moment");
 const {
   time,
   loadFixture,
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+
+const TWEI = 1_000_000_000_000;
+const RATE = 100*TWEI;
 
 describe("Lock2", function () {
   // We define a fixture to reuse the same setup in every test.
@@ -13,8 +17,7 @@ describe("Lock2", function () {
     const [owner, alice, bob] = await ethers.getSigners();
 
     const Lock2 = await ethers.getContractFactory("Lock2");
-    const lock2 = await Lock2.deploy();
-
+    const lock2 = await Lock2.deploy(RATE);
     return { lock2, owner, alice, bob };
   }
 
@@ -22,13 +25,57 @@ describe("Lock2", function () {
     it("Should set the right host", async function () {
       const { lock2, owner, alice, bob} = await loadFixture(deployFixture);
 
-      await expect(lock2.setHost('a')).to.be.revertedWith("host length greater than 1");
-      await expect(lock2.setHost('abc')).not.to.be.reverted;
-      expect(await lock2.host()).to.equal('abc');
+      //const now = await time.latest();
+      //const expiredAt = moment(now*1000).add(3, 'months').add(1, 'days').startOf('day').unix();
+      await expect(lock2.setHost('a', 1)).to.be.revertedWith("host length must be greater than 1");
 
-      await expect(lock2.connect(alice).setHost('xyz')).not.to.be.reverted;
-      expect(await lock2.host()).to.equal('xyz');
-      expect(await lock2.owner()).to.equal(alice.address);
+      const days = 31;
+
+      {
+        await expect(lock2.connect(alice).setHost('alice-host', days, {
+          //from: alice.address,
+          value: days*RATE
+        })).not.to.be.reverted;
+    
+        let {host:_host, owner:_owner} = await lock2.hosts('alice-host');
+        expect(_host).to.equal('alice-host');
+        expect(_owner).to.equal(alice.address);
+      }
+
+      {
+        await expect(lock2.connect(bob).setHost('bob-host', days, {
+          //from: bob.address,
+          value: days*RATE
+        })).not.to.be.reverted;
+    
+        let {host:_host, owner:_owner} = await lock2.hosts('bob-host');
+        expect(_host).to.equal('bob-host');
+        expect(_owner).to.equal(bob.address);
+      }
+
+      {
+        await expect(lock2.connect(bob).setHost('alice-host', days, {
+          //from: bob.address,
+          value: days*RATE
+        })).to.be.revertedWith("You aren't the owner and record is not expired yet");
+    
+        const unlockTime = (await time.latest()) + days*60*60*24;
+        await time.increaseTo(unlockTime);
+
+        await expect(lock2.connect(bob).setHost('alice-host', days, {
+          //from: bob.address,
+          value: days*RATE
+        })).not.to.be.reverted;
+
+        let {host:_host, owner:_owner, expiredAt} = await lock2.hosts('alice-host');
+        expect(_host).to.equal('alice-host');
+        expect(_owner).to.equal(bob.address);
+
+        const numOfDays = Math.floor(moment.duration(moment(expiredAt.toNumber()*1000).diff(moment())).as('days'));
+        expect(numOfDays).to.equal(2 * days);
+
+        expect(await ethers.provider.getBalance(lock2.address)).to.equal(ethers.BigNumber.from(RATE).mul(3 * days));
+      }
     });
   });
 });
