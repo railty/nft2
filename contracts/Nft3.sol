bc36b89ece5f959d3c72e1c81b3eef114e40b66f
Shawn Ning <zxning@gmail.com>
2022-09-21 23:26:56 -0400

    update

--- contracts/Nft3.sol
+++ contracts/Nft3.sol
@@ -4,18 +4,20 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 struct Rec { 
     uint256 tokenId;
-    address owner;
     uint expiredAt;
 }
 
-contract Nft3 is ERC721, Ownable {
+contract Nft3 is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
+    using ECDSA for bytes32;
     Counters.Counter private _tokenIds;
     mapping(uint256 => string) private _tokenURIs;
     
@@ -48,25 +50,86 @@ contract Nft3 is ERC721, Ownable {
 
             if (block.timestamp > hosts[host].expiredAt){
                 //expired 
+                delete ids[hosts[host].tokenId];
                 _burn(hosts[host].tokenId);
+
                 newItemId = _mintNFT();
-                hosts[host] = Rec(newItemId, msg.sender, block.timestamp + numOfDays*24*60*60);
+                hosts[host] = Rec(newItemId, block.timestamp + numOfDays*24*60*60);
+                ids[newItemId] = host;
             }
             else{
-                require(msg.sender == hosts[host].owner, "You aren't the owner and record is not expired yet");
+                require(msg.sender == ownerOf(hosts[host].tokenId), "You aren't the owner and record is not expired yet");
 
+                delete ids[hosts[host].tokenId];
                 _burn(hosts[host].tokenId);
                 newItemId = _mintNFT();
-                hosts[host] = Rec(newItemId, msg.sender, hosts[host].expiredAt + numOfDays*24*60*60);
+                hosts[host] = Rec(newItemId, hosts[host].expiredAt + numOfDays*24*60*60);
+                ids[newItemId] = host;
             }
         }
         else{
             console.log("new record");
 
             newItemId = _mintNFT();
-            hosts[host] = Rec(newItemId, msg.sender, block.timestamp + numOfDays*24*60*60);
+            hosts[host] = Rec(newItemId, block.timestamp + numOfDays*24*60*60);
+            ids[newItemId] = host;
         }
 
         return newItemId;
+    }
+
+    function lastId() public view returns (uint256) {
+        return _tokenIds.current();    
+    }
+
+    function verify(bytes32 smsg, bytes memory sig) public view returns (address) {
+        
+        address x = smsg.recover(sig);
+        console.log("x=", x);
+        return x;
+    }
+
+
+    function messageHash(bytes memory message) public pure returns(bytes32) {
+        return keccak256(message);
+    }
+
+    function ethSignedHash(bytes32 hMessage) public pure returns(bytes32) {
+        return hMessage.toEthSignedMessageHash();
+    }
+
+    function recover(bytes32 hash, bytes memory signature) public pure returns(address) {
+        return hash.recover(signature);
+    }
+
+    function bytes32ToBytes(bytes32 _bytes32) internal pure returns (bytes memory){
+        bytes memory bytesArray = new bytes(32);
+        for (uint256 i; i < 32; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return bytesArray;
+    }
+
+    function nonce() public view returns(bytes32) {
+        return keccak256(abi.encodePacked(block.number));
+    }
+
+    //the problem is nonce, which created from solidity and return to js, the next call need use this nonce, 
+    //which means we need save the nonce somewhere on chain, which is too expensive
+    function verify(bytes memory signature) public view returns (address) {
+        bytes32 msgHash = keccak256(
+            abi.encode(nonce(), msg.sender, 1234, "hello world")
+        );
+        
+        console.log("msgHash=", string(bytes32ToBytes(msgHash)));
+
+        bytes32 signedMsgHash = msgHash.toEthSignedMessageHash();
+        
+        console.log("msgHash=", string(bytes32ToBytes(signedMsgHash)));
+
+        address addr = signedMsgHash.recover(signature);
+
+        
+        return addr;
     }
 }
--- test/Nft3.js
+++ test/Nft3.js
@@ -8,7 +8,7 @@ const { expect } = require("chai");
 const TWEI = 1_000_000_000_000;
 const RATE = 100*TWEI;
 
-describe("Nft2", function () {
+describe("Nft3", function () {
   async function deployFixture() {
     const [owner, alice, bob] = await ethers.getSigners();
 
@@ -66,19 +66,130 @@ describe("Nft2", function () {
       const unlockTime2 = (await time.latest()) + (days+20)*60*60*24;
       await time.increaseTo(unlockTime2);
 
-      //bob can not register as it is expired
+      //bob can now register as it is expired
       const rec1 = await nft3.hosts('alice-host');
-      expect(rec1.owner).to.equal(alice.address);
       await expect(nft3.connect(bob).mintNFT('alice-host', days, {
         value: ethers.BigNumber.from(RATE).mul(days)
       })).not.to.be.reverted;
       const rec2 = await nft3.hosts('alice-host');
-
       expect(rec1.tokenId).not.to.equal(rec2.tokenId);
-      expect(rec2.owner).to.equal(bob.address);
 
       const tm = rec2.expiredAt - rec1.expiredAt;
       expect(Math.floor(tm/60/60/24)).to.equal(days+10);
+
+      const lastId = await nft3.lastId();
+      console.log(lastId);
+      for (let i=0; i<=lastId; i++){
+        try{
+          const owner = await nft3.ownerOf(i);
+          const host = await nft3.ids(i);
+          console.log(i, owner, host);
+        }
+        catch(e){
+          console.log(i, e.reason);
+        }
+      }
+      console.log("-------------------");
+
+
+      //alice cannot renew it as it belongs to bob now
+      await expect(nft3.connect(alice).mintNFT('alice-host', days, {
+        value: ethers.BigNumber.from(RATE).mul(days)
+      })).to.be.revertedWith("You aren't the owner and record is not expired yet");
+
+      //transfer the nft to alice
+      await nft3.connect(bob).transferFrom(bob.address, alice.address, 4);
+
+      for (let i=0; i<=lastId; i++){
+        try{
+          const owner = await nft3.ownerOf(i);
+          const host = await nft3.ids(i);
+          console.log(i, owner, host);
+        }
+        catch(e){
+          console.log(i, e.reason);
+        }
+      }
+
+      //alice can renew it again
+      await expect(nft3.connect(alice).mintNFT('alice-host', days, {
+        value: ethers.BigNumber.from(RATE).mul(days)
+      })).not.to.be.reverted;
+
+      const nAlice = await nft3.balanceOf(alice.address);
+      const nBob = await nft3.balanceOf(bob.address);
+
+      console.log(nAlice, nBob);
+
+      for (let i=0; i<nAlice; i++){
+        const tokenId = await nft3.tokenOfOwnerByIndex(alice.address, i);
+        console.log("Alice token = ", tokenId);
+      }
+
+      for (let i=0; i<nBob; i++){
+        const tokenId = await nft3.tokenOfOwnerByIndex(bob.address, i);
+        console.log("Bob token = ", tokenId);
+      }
+
+/*
+      const msg = "1234abcd";
+      const hmsg = ethers.utils.hashMessage(msg);
+
+      let sig = await alice.signMessage(hmsg);
+      console.log("sig=", sig);
+      sig = sig.substr(0, 130) + (sig.substr(130) == "00" ? "1b" : "1c"); // v: 0,1 => 27,28
+      console.log("sig=", sig);
+
+      const rc = await nft3.connect(alice).verify(hmsg, sig);
+      console.log("rc=", rc);
+      expect(rc).to.equal(alice.address);
+
+
+      var message = "0x1234";
+      console.log("message: ", message);
+      var msgHash    = await nft3.messageHash(message);
+      var ethHash = await nft3.ethSignedHash(msgHash);
+  
+      let signature = await alice.signMessage(message);
+      //signature = signature.substr(0, 130) + (signature.substr(130) == "00" ? "1b" : "1c"); // v: 0,1 => 27,28
+      var recovered = await nft3.recover(ethHash, signature); // recover from the ethHash
+      console.log(recovered, alice.address);
+      console.log(ethHash, signature);
+
+      const message = ethers.utils.toUtf8Bytes("hello world");
+      const messageHash = ethers.utils.keccak256(message);
+      console.log("messageHash", messageHash);
+
+      let signature = await alice.signMessage(messageHash);
+      console.log("signature", signature);
+
+      let address = await ethers.utils.verifyMessage(messageHash, signature);
+      console.log("address", address);
+      expect(address).to.be.equal(alice.address);
+
+      const signature2 = signature.substr(0, 130) + (signature.substr(130) == "00" ? "1b" : "1c"); // v: 0,1 => 27,28
+      var recovered = await nft3.recover(messageHash, signature2);
+      console.log("recovered", recovered);
+
+      var recovered2 = await nft3.test();
+
+*/      
+      const nonce = await nft3.nonce()
+      console.log("nonce:", nonce);
+      const msg1 = ethers.utils.keccak256(
+        ethers.utils.defaultAbiCoder.encode(['uint256', 'address', 'uint256',  'string'], [nonce, alice.address, 1234, "hello world"])
+      );
+      const msg2 = ethers.utils.arrayify(msg1);
+
+      console.log("msg1=", msg1);
+      console.log("msg2=", msg2);
+
+      const signature = await alice.signMessage(msg2);
+
+      await time.increase(2000);
+      
+      var recovered2 = await nft3.connect(alice).test(signature);
+      expect(recovered2).to.be.equal(alice.address);
     });
   });
 });
